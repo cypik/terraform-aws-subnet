d61218ffd522aaef1d56b98a7686933b6225b722
cypikmahesh <mahesh.y@cypik.com>
2023-12-01 13:04:33 +0530

    add database subnet outputs

--- _example/database-subnet/outputs.tf
+++ _example/database-subnet/outputs.tf
@@ -1,19 +1,19 @@
-output "public_subnet_cidrs" {
-  value       = module.subnet.public_subnet_cidrs
+output "database_subnet_cidrs" {
+  value       = module.subnet.database_subnet_cidrs
   description = "The CIDR of the subnet."
 }
 
-output "public_subnet_cidrs_ipv6" {
-  value       = module.subnet.public_subnet_cidrs_ipv6
+output "database_subnet_cidrs_ipv6" {
+  value       = module.subnet.database_subnet_cidrs_ipv6
   description = "The CIDR of the subnet."
 }
 
-output "public_tags" {
-  value       = module.subnet.public_tags
+output "database_tags" {
+  value       = module.subnet.database_tags
   description = "A mapping of tags to assign to the resource."
 }
 
-output "public_subnet_id" {
-  value       = module.subnet.private_subnet_id
+output "database_subnet_id" {
+  value       = module.subnet.database_subnet_id
   description = "The ID of the public subnet"
 }
